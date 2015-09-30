29ca246e7706f485b2079e93e64f4a61abbc9970
Brad <Brad@Brad-PC-Surface>
2015-09-30 16:26:14 -0600

    Cleaned up the code and added try/catch blocks.

--- CS246_03.4/src/cs246_03/pkg4/CS246_034.java
+++ CS246_03.4/src/cs246_03/pkg4/CS246_034.java
@@ -1,57 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs246_03.pkg4;
 
 import java.io.IOException;
-import org.w3c.dom.Document;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
- *
- * @author Brad, Brandon
+ * CS 246 03.4 Team Activity: Parsing XML
+ * @author Brad Bodily, Brandon Hartshorn
  */
 public class CS246_034 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-        // TODO code application logic here
+    public static void main(String[] args) {
         
         CS246_034 main = new CS246_034();
-        main.loadFile("conference.xml");
-        
+        main.parseConferenceFile("conference.xml");   
     }
     
     /**
-     * 
-     * @param filename
-     * @throws ParserConfigurationException
-     * @throws SAXException
-     * @throws IOException 
+     * Attempts to parse the specified file and display all speaker names.
+     * @param filename  filename to parse
      */
-    private void loadFile(String filename) throws ParserConfigurationException, SAXException, IOException {
+    private void parseConferenceFile(String filename) {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        
-        Document doc = builder.parse("conference.xml");
-        
-        NodeList sessions = doc.getElementsByTagName("speaker");
-        
-        for (int i = 0; i < sessions.getLength(); i++) {
-            Element speaker = (Element) sessions.item(i);
-            String speakerName = speaker.getAttribute("name");
-            System.out.println(speakerName);
+                
+        // Attemp to create new DocumentBuilder
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
             
+            // Attempt to parse file
+            try {
+                // Parse
+                Document doc = builder.parse(filename);
+                
+                // Get speaker elements
+                NodeList sessions = doc.getElementsByTagName("speaker");
+
+                for (int i = 0; i < sessions.getLength(); i++) {
+                    Element speaker = (Element) sessions.item(i);
+                    String speakerName = speaker.getAttribute("name");
+                    System.out.println(speakerName);
+                }                 
+                
+            } catch (SAXException e) {
+                System.err.format("SAXException exception. Cannot continue.");
+                System.exit(1);
+            } catch (IOException e) {
+                System.err.format("IOException parsing '%s'. Cannot continue.", filename);
+                System.exit(1);                
+            }
+        } catch (ParserConfigurationException e) {
+            System.err.format("Parser configuration error. Cannot continue.\n");
+            System.exit(1);
         }
-    }
-    
+    }   
 }
