e69a34519e1975e3e680d2b067b92d75459cf7c0
Brad <Brad@Brad-PC-Surface>
2015-09-29 21:17:23 -0600

    Create document

--- CS246_03.4/src/cs246_03/pkg4/CS246_034.java
+++ CS246_03.4/src/cs246_03/pkg4/CS246_034.java
@@ -5,6 +5,14 @@
  */
 package cs246_03.pkg4;
 
+import java.io.IOException;
+import org.w3c.dom.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 /**
  *
  * @author Brad
@@ -14,8 +22,23 @@ public class CS246_034 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
         // TODO code application logic here
+        System.out.println("Starting");
+        
+        CS246_034 main = new CS246_034();
+        main.loadFile("conference.xml");
+        
+        System.out.println("Done");
+    }
+    
+    private void loadFile(String filename) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        
+        Document doc = builder.parse("conference.xml");
+        
+        NodeList sessions = doc.getElementsByTagName("speaker");
     }
     
 }
