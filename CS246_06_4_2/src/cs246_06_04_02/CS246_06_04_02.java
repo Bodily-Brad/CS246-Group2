ff109dbc1334d345edcc8d69fd0ef558afe93b00
Bodily-Brad <bod12002@byui.edu>
2015-10-19 15:00:26 -0600

    Team 06.4.2 Initial Commit

---
+++ CS246_06_4_2/src/cs246_06_04_02/CS246_06_04_02.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs246_06_04_02;
+
+/**
+ *
+ * @author bradb
+ */
+public class CS246_06_04_02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CountByEvens evenJob = new CountByEvens();
+        CountByOdds oddJob = new CountByOdds();
+        
+        Thread evenThread = new Thread(evenJob);
+        Thread oddThread = new Thread(oddJob);
+        
+        evenThread.start();
+        oddThread.start();
+    }
+    
+}
